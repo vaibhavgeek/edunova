19da307922b5950fb010ea0949fb07c7c2648c64
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2016-03-08 02:31:16 +0530

    finale

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -121,6 +121,7 @@ end
 
   def show
    @note = Note.friendly.find(params[:id])
+   @questions_hash = JSON.parse(@note.questions)
    @get_level = Play.where(:user_id => current_user.id , :note_id => @note).first
   end
   def comment_view  
